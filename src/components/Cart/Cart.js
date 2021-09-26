bdf80cd6e9e14307ba0dfed7abd8c2d98129e17f
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-09-26 22:22:41 +0600

    cart total added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,23 +3,30 @@ import './Cart.css'
 
 const Cart = (props) => {
     const {cart,name} = props;
+    console.log(name)
+    
     let total= 0 ;
     for(const pharmaceutical of cart){
+        // console.log(pharmaceutical.name)
         total =total + pharmaceutical.value;
     }
+
+    let Name ='';
+    for (const names of cart){
+        console.log(names.name)
+        Name = Name + names.name;
+    }
+
 
     
     return (
         <div className="cart-details pt-2">
             <h3 className="add-company">Company Cart</h3>
             <h5>Company added: {cart.length}</h5>
-            <p>Total value (Cr. Taka): {total.toFixed(2)}</p>
-            <p>
-                <li>
-                    <ul>Company: {name}</ul>
-                </li>
-            </p>
-
+            <p className="total-value">Total value (Cr. Taka): {total.toFixed(2)}</p>
+            <div>
+                <ul><p>Name:{Name}</p></ul>
+            </div>
             
         </div>
     );
